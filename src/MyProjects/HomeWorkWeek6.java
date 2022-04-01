49b456a960e2a3c269b055fd1d809d82e7c848cf
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-01 17:14:58 -0500

    Session3

---
+++ src/MyProjects/HomeWorkWeek6.java
@@ -0,0 +1,25 @@
+package MyProjects;
+
+import java.util.Scanner;
+
+public class HomeWorkWeek6 {
+    public static void main(String[] args) {
+
+        //Create a Java Program to Swap Two Numbers
+
+
+        int i = 77;
+        int ii = 55;
+
+        System.out.println("BEFORE SWAP: " + i + " , " +ii);
+
+        System.out.println("AFTER SWAP: " + ii + " , " +i);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/StudyHallSessions/Week5/Session3.java
@@ -0,0 +1,19 @@
+package StudyHallSessions.Week5;
+
+public class Session3 {
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i <=10 ; i++){
+            System.out.println("------------------------");
+            System.out.println("TABLE OF : " +i);
+            System.out.println("------------------------");
+
+            for (int k =1 ;  k<=10 ; k++){
+                System.out.println(i + " x " +k + " = " + (i*k));
+
+            }
+        }
+
+
+    }
+}
