ee6d3b7e929a03c08c184cf2e1466404559305f7
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-01 17:31:35 -0500

    Session3

--- src/StudyHallSessions/Week5/Session3.java
+++ src/StudyHallSessions/Week5/Session3.java
@@ -3,16 +3,32 @@ package StudyHallSessions.Week5;
 public class Session3 {
     public static void main(String[] args) {
 
-        for (int i = 1 ; i <=10 ; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println("------------------------");
-            System.out.println("TABLE OF : " +i);
+            System.out.println("TABLE OF : " + i);
             System.out.println("------------------------");
 
-            for (int k =1 ;  k<=10 ; k++){
-                System.out.println(i + " x " +k + " = " + (i*k));
+            for (int k = 1; k <= 10; k++) {
+                System.out.println(i + " x " + k + " = " + (i * k));
 
             }
         }
+        System.out.println("***********************************************");
+
+        //write a program to creat a Right Triangle Star Pattern Program
+        //Please use nested for loop and height is 7.
+
+        for (int j=1 ; j<=7 ; j++){
+
+            for (int k =1 ; k<=j ; k++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+
+        }
+
 
 
     }
