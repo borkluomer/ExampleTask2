1895b9292a10aa4d1e17f60f6500586180c71e53
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-02 18:15:03 -0500

    CALCULATOR -+/*

--- src/MyProjects/HomeWorkWeek6.java
+++ src/MyProjects/HomeWorkWeek6.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 public class HomeWorkWeek6 {
     public static void main(String[] args) {
 
-        //Create a Java Program to Swap Two Numbers
+        //Create a Java Program to Make a Simple Calculator Using switch...case
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("ENTER FIRST NUMBER : ");
+        double d1 = scan.nextDouble();
+
+        System.out.println("ENTER YOUR SECOND NUMBER : ");
+        double d2= scan.nextDouble();
+        System.out.println("YOU CALC : ");
+
+        char calc = scan.next().charAt(0);
+        double result = 0;
+        switch (calc){
+            case'+':
+                result= d1 + d2;
+                break;
+            case'-':
+                result= d1 - d2;
+                break;
+            case'*':
+                result= d1 * d2;
+            case'/':
+                result= d1 / d2;
+            default:
+                System.out.println("WRONG SYSTEM USED TRY THE RIGHT TERMS SUCH AS (+,-,/,*) TO DO YOUR CALC ");
+
+        }
+        System.out.println(d1+ " " +calc + " " +d2 + " " + result);
 
 
-        int i = 77;
-        int ii = 55;
-
-        System.out.println("BEFORE SWAP: " + i + " , " +ii);
-
-        System.out.println("AFTER SWAP: " + ii + " , " +i);
 
 
 
