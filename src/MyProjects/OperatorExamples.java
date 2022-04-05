c8e0a3721a716c78499b2bcfa451f8fc437482bd
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-05 14:44:48 -0500

    OperatorExample

---
+++ src/MyProjects/OperatorExamples.java
@@ -0,0 +1,43 @@
+package MyProjects;
+
+public class OperatorExamples {
+    public static void main(String[] args) {
+
+        //relation operators of JAVA
+        // == is equal to
+        //> greater than
+        //< less than
+        //>= Greater that or equal to
+        //<= Less that or equal to
+
+        // == is equal to
+        boolean result1 = 55 == 25; // false
+        boolean result2 = 55 == 55; // true
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        //!= not equal to
+        System.out.println(20!=20); //false
+        System.out.println(10!=20); //true
+
+        //> greater than
+        System.out.println(10>1); //true
+        boolean result3 = 10>100; //false
+        System.out.println(result3); //false
+
+        //< less than
+        System.out.println(2<5); //true
+        System.out.println(2<2); //false they not equal
+
+        //>= Greater that or equal to
+        System.out.println(2>=2); //true
+        System.out.println(2>=0); //true
+
+
+
+
+
+
+    }
+}
