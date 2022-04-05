06f97d749284a85a9270d5942406ff239166e3b4
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-05 15:56:59 -0500

    AssigmentOperatorsExample

---
+++ src/MyProjects/AssigmentOperatorsExample.java
@@ -0,0 +1,68 @@
+package MyProjects;
+
+public class AssigmentOperatorsExample {
+    public static void main(String[] args) {
+
+        //  = equal              a=b;       a = b;
+
+        // += plus equal         a+=b;      a=a+b;
+
+        // -= minus equal        a-=b;      a=a-b;
+
+        // *= multiply equal     a*+b;      a=a*b;
+
+        // %= devide equal       a%=b;      a=a%b;
+
+
+        //= assigment
+        int x = 20;
+        x=30; //assing new 30 value to x variable
+
+        System.out.println(x);
+
+        //+= assigment
+        int z =20;
+        z+=30; //it is going to increase the z value by 30 its like z=20+30 so z is 50 now
+        System.out.println(z); //50
+
+        //-= assigment basically subracting
+        int number1 = 10;
+        number1 -= 1; //basically you are saying number = number-1
+        System.out.println(number1); //result will be 9
+
+        //*= operator
+        int number2 = 10;
+        number2 *=2; // number2=number2*2
+        System.out.println(number2); //result will be 20
+
+        // /= operator
+
+        int number3= 100;
+        number3 /=2; //number3 = number3/2 so result will be 50
+        System.out.println(number3);
+
+        //%=
+        int number4= 200;
+        number4 %=5; // means number4= number4 % 5
+        System.out.println(number4); //result will be 0 we look for the remaining for the result
+        //if there is any ramining number after the devision that will be the value so in this case its 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
