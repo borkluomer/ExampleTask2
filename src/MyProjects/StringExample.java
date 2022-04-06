f31cd6b354a6755829fe92e937f30180b37b042f
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-06 00:27:46 -0500

    StringExample

---
+++ src/MyProjects/StringExample.java
@@ -0,0 +1,27 @@
+package MyProjects;
+
+public class StringExample {
+    public static void main(String[] args) {
+
+        /*
+        * in java , string is an object that represents a sequence of characters.
+        * the java.lang.String class is used to create a string object.
+        *
+        * how to create a string object?
+        * there are two ways to create String objects:
+        *
+        * 1. By String literal
+        * 2. By new keyword
+        *
+        * create a string object
+        * 1.By string literal is created by using double qoutes.
+        * for example:
+        * String str1="Hello"
+        *
+        */// to be continued...!!!!!!!
+
+
+
+
+    }
+}
