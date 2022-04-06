54124f0a1d867663a94cd56732923d2c50da5cdd
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-05 21:05:25 -0500

    c3_ArraysPractice

--- src/day16_Arrays/c3_ArraysPractice.java
+++ src/day16_Arrays/c3_ArraysPractice.java
@@ -40,11 +40,44 @@ public class c3_ArraysPractice {
         System.out.println("******************************");
 
         System.out.println(Arrays.toString(biggestToSmallest)); // [99, 90, 50, 34, -90]
+        System.out.println("******************************************************************");
 
 
         ////write a program that can return the largest string of text from an array
-        ////ex         String[] names = {"denver", "texas", "orlando", "Toronto", "jeniffer", "sevgin", "renastechschool"};
+        ////ex  String[] names = {"denver", "texas", "orlando", "Toronto", "jeniffer", "sevgin", "renastechschool"};
         ////output :renastechschool
+
+        // length =           6        5         4         3          2            1          0
+        String [] names ={"denver", "texas", "orlando", "Toronto", "jeniffer", "sevgin", "renastechschool"};
+                         // 0         1        2          3            4          5          6
+        //length of the index lets find that first
+        System.out.println(names[2].length()); //length is 7
+
+        int maxLengthIndex=names[0].length(); //this is assuming the names index 0 has the longest just guessing
+
+        for (int i = 0; i < names.length ; i++) {
+            //i=0         0<7              i++
+            //i=1         1<7              i++
+            System.out.println(" " + names[i] + " has "+ names[i].length() + " is length" );
+
+
+            //1     names[0].length 6 >6
+            //2     names[1].length 5 >6
+            //3     names[2].length 4 >6
+            //4     names[3].length 3 >6
+            //5     names[4].length 2 >6
+            //6     names[5].length 1 >6
+
+            if (names[i].length()>maxLengthIndex){
+                maxLengthIndex=i;
+
+            }
+
+
+        }
+        System.out.println(names[maxLengthIndex]);
+
+
 
 
 
