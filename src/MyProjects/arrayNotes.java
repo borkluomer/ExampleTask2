49416ae79b8ec09c63fd689e973ed7efb2492e98
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-07 13:02:33 -0500

    arrayNotes

---
+++ src/MyProjects/arrayNotes.java
@@ -0,0 +1,64 @@
+package MyProjects;
+
+public class arrayNotes {
+    public static void main(String[] args) {
+
+        //Q1 What is an Array?
+        // Arrays are basically a Storage where you store similar stuff in a group to organize.
+
+        //Q2 How do you declare an Array in java?
+        //There is basically a syntax of array like every other topic which contains
+        //Data Type then square brackets then you give a name of the variable whatever the group youre trying to create
+        //then basically set the other side of the equal sign with "text" with curly brackets ;
+        //such has String [] variable = new String []
+
+        //Q3 What is the default value of Array for different data types?
+        // int 0
+        //boolean false return
+        //string will return as null
+
+
+        //Q4 Can you change size of Array in java after creation?
+        //no you cant change it
+
+        //Q5 Can you pass the negative number in Array size?
+        //no we cant
+
+
+
+
+       //Q6 Can you declare an Array without Array size?
+        //Array needs size, so you can't declare an array without a size
+
+       //Q7 Where does Array stored in JVM memory ?
+        //it's Stored in Heap Memory
+
+       //Q8 Given a primitive Array in java, where does in JVM it is stored?
+        //Heap
+
+       //Q10 What is the difference between ArrayStoreException and ArrayOutOfBoundsException ?
+        // ArrayStoreException will come on when you're trying to store none similar data's together
+        //such as string with int ...also ArrayOutOfBoundsException will appear on print section when youre out of
+        //your range
+
+       //Q11 What are the advantages of Array ?
+        //easier access to a group of number,names,characters it makes easier to make chances or cancellation
+        //for future projects
+
+       //Q12 What are the disadvantages of Array?
+        // 1. waste of memory down the road
+        // 2. its little bit more complicated to set vs any other syntax in my opinion
+        // 3. sizes are fixed
+        // 4. basically limited and its not flexible
+        // 5.
+
+
+
+
+
+
+
+
+
+    }
+}
