e02d0ba0496a347c96a563a76b0e4f067ff74ad0
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-08 19:52:50 -0500

    c3_ForEachLoop

--- src/day18_ForEachLoop/c3_ForEachLoop.java
+++ src/day18_ForEachLoop/c3_ForEachLoop.java
@@ -9,6 +9,34 @@ public class c3_ForEachLoop {
         //do while
         //for each loop
 
+        int [] numbers = {1,2,3,4,5,6};
+
+        for (int eachNumber : numbers){
+            System.out.println(eachNumber);
+        }
+
+        System.out.println("------------------------------------");
+
+        //for each loop will store each variable from the numbers list
+        //in each ittiration from first one to last one
+
+        String [] initials = {"OCB", "BK" , "HB", "CEB" , "MTB"};
+        // print all initials using for each
+
+        for ( String x : initials){
+            System.out.println(x);
+        }
+
+        System.out.println("----------------------------------------");
+
+
+        String sentence = "Hello world java is here";
+        String [] words = sentence.split("");
+
+        for (String word : words){
+            System.out.println(word);
+        }
+
 
 
 
