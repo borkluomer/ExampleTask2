937ffdb3106a73b54ec73b801afd7d7130145600
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-11 20:48:10 -0500

    c4_LibraryTest

---
+++ src/day19_methods/c4_LibraryTest.java
@@ -0,0 +1,12 @@
+package day19_methods;
+
+import MyUtils.GenerateUtils;
+
+public class c4_LibraryTest {
+    public static void main(String[] args) {
+
+        GenerateUtils.hello();
+        GenerateUtils.reverseString("java");
+        GenerateUtils.reverseString("kayak");
+    }
+}
