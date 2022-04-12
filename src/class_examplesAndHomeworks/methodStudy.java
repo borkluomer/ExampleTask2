fadd361f213fa84b083cc3cbfe909ca5a59498be
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-12 02:08:33 -0500

    update

---
+++ src/class_examplesAndHomeworks/methodStudy.java
@@ -0,0 +1,18 @@
+package class_examplesAndHomeworks;
+
+public class methodStudy {
+    public static void main(String[] args) {
+
+     addToCart();
+    }
+
+    public static void addToCart(){
+        System.out.println("ADDED TO CART");
+    }
+
+
+
+
+
+
+}
